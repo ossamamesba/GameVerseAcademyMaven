4603fe3657aa26705b417075a79096831201e458
Aymen Benrbib <aymen.benrbib@esi.ac.ma>
2026-04-23 22:35:01 +0100

    Update database connection details in DBUtil
    Working with a cloud service instead of a simple local host DB

--- src/main/java/ma/ac/esi/gameverseacademy/util/DBUtil.java
+++ src/main/java/ma/ac/esi/gameverseacademy/util/DBUtil.java
@@ -1,15 +1,11 @@
 package ma.ac.esi.gameverseacademy.util;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class DBUtil {
-
-    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "root";
-
+    private static final String URL = "jdbc:postgresql://dpg-d7l35vm47okc73b5p6qg-a.frankfurt-postgres.render.com:5432/gameverseacademy_pkye";
+    private static final String USER = "gameverseacademy_pkye_user";
+    private static final String PASSWORD = "xILxTNTLw0ZzHnSYQ00bkqyeaTl9clui";
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("org.postgresql.Driver");
